4bafa3b5839a6ba9f088aaa827cce8090f841c8c
Alx370 <alessandro.formicola@edu-its.it>
2025-06-12 12:07:33 +0200

    Progetto Java Rifugio Animali
    Progetto dell'applicazione per il rifugio Animali richiesto dal cliente

---
+++ Rifugio_Animali/src/test/java/com/catarsi/Rifugio_Animali/RifugioAnimaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catarsi.Rifugio_Animali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifugioAnimaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
