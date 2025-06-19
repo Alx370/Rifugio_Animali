3136f0a8811bba6162b67c8017148ffce865b95c
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-06-19 18:32:30 +0200

    Creazione cartella controller + creazione del RifugioAnimaliMVC.java

---
+++ src/main/java/com/catarsi/Rifugio_Animali/controller/RifugioAnimaliMVC.java
@@ -0,0 +1,27 @@
+package com.catarsi.Rifugio_Animali.controller;
+
+import com.catarsi.Rifugio_Animali.repos.RifugioRepoAnimale;
+import com.catarsi.Rifugio_Animali.services.RifugioServiceAnimali;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RifugioAnimaliMVC {
+
+    private final RifugioRepoAnimale repoAnimale;
+
+    @Autowired
+    private RifugioServiceAnimali srvAnimale;
+
+    public RifugioAnimaliMVC(RifugioRepoAnimale repoAnimale) {
+        this.repoAnimale = repoAnimale;
+    }
+
+    @GetMapping("Animali")
+    public String addAnimali(Model m) {
+        m.addAttribute("animali", srvAnimale.getAnimali());
+        return "animali";
+    }
+}
