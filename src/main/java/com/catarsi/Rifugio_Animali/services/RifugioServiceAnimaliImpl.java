e8bf0924273917b4efb52f247aefd5640909054b
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-06-19 17:19:56 +0200

    Creazione dell'RifugioServiceAnimaliImpl.java + aggiunta attributo al metodo per Aggiungere un animale

---
+++ src/main/java/com/catarsi/Rifugio_Animali/services/RifugioServiceAnimaliImpl.java
@@ -0,0 +1,25 @@
+package com.catarsi.Rifugio_Animali.services;
+
+import com.catarsi.Rifugio_Animali.model.Animale;
+import com.catarsi.Rifugio_Animali.repos.RifugioRepoAnimale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RifugioServiceAnimaliImpl implements RifugioServiceAnimali {
+
+    @Autowired
+    private RifugioRepoAnimale animali_repo;
+
+    @Override
+    public Animale addAnimale(Animale a) {
+        return animali_repo.save(a);
+    }
+
+    @Override
+    public List<Animale> getAnimali() {
+        return animali_repo.findAll();
+    }
+}
