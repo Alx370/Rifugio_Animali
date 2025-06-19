0a5f7d2a71c030a08235958e5bdd030d393682fd
daniela <daniela.punzi@edu-its.it>
2025-06-19 17:14:54 +0200

    creazione interface RifugioServiceDottore

---
+++ src/main/java/com/catarsi/Rifugio_Animali/services/RifugioServiceDottore.java
@@ -0,0 +1,11 @@
+package com.catarsi.Rifugio_Animali.services;
+
+import java.util.List;
+
+import com.catarsi.Rifugio_Animali.model.Dottore;
+
+public interface RifugioServiceDottore {
+    
+    Dottore addDottore();
+    List<Dottore> addDottori();
+}
