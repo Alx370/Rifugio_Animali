34319c8f3bb6975ca1999f395898a4d18552fb9a
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-06-19 18:13:52 +0200

    Creazione interfaccia RifugioServiceVisita.java + classe RifugioServiceVisitaImpl.java e implementazione dei metodi

---
+++ src/main/java/com/catarsi/Rifugio_Animali/services/RifugioServicesVisitaImpl.java
@@ -0,0 +1,25 @@
+package com.catarsi.Rifugio_Animali.services;
+
+import com.catarsi.Rifugio_Animali.model.Visita;
+import com.catarsi.Rifugio_Animali.repos.RifugioRepoVisita;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RifugioServicesVisitaImpl implements RifugioServicesVisita{
+
+    @Autowired
+    private RifugioRepoVisita visita_repo;
+
+    @Override
+    public Visita addVisita(Visita v) {
+        return visita_repo.save(v);
+    }
+
+    @Override
+    public List<Visita> getVisite() {
+        return visita_repo.findAll();
+    }
+}
