8285ac82076b4decf11b62c4b5daae876aa3e921
daniela <daniela.punzi@edu-its.it>
2025-06-21 15:15:06 +0200

    Correzione del model di Visita

--- src/main/java/com/catarsi/Rifugio_Animali/model/Visita.java
+++ src/main/java/com/catarsi/Rifugio_Animali/model/Visita.java
@@ -2,33 +2,43 @@ package com.catarsi.Rifugio_Animali.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
 
 @Entity
+@Table(name = "visita")
 public class Visita {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    @Column(name = "Id_visita")
     private int id_visita;
 
     @ManyToOne
-    @JoinColumn(name="Animale")
-    private int id_animale;
+    @JoinColumn(name = "Id_animale", nullable = false)
+    private Animale animale;
 
-    @OneToMany
-    @JoinColumn(name="Dottore")
-    private int id_dottore;
+    @ManyToOne
+    @JoinColumn(name = "Id_dottore", nullable = false)
+    private Dottore dottore;
 
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_visita", nullable = false)
     private Date data_visita;
 
+    @Column(name = "descrizione", columnDefinition = "TEXT")
     private String descrizione;
+
+    public Visita() {}
 
     public int getId_visita() {
         return id_visita;
@@ -38,20 +48,20 @@ public class Visita {
         this.id_visita = id_visita;
     }
 
-    public int getId_animale() {
-        return id_animale;
+    public Animale getAnimale() {
+        return animale;
     }
 
-    public void setId_animale(int id_animale) {
-        this.id_animale = id_animale;
+    public void setAnimale(Animale animale) {
+        this.animale = animale;
     }
 
-    public int getId_dottore() {
-        return id_dottore;
+    public Dottore getDottore() {
+        return dottore;
     }
 
-    public void setId_dottore(int id_dottore) {
-        this.id_dottore = id_dottore;
+    public void setDottore(Dottore dottore) {
+        this.dottore = dottore;
     }
 
     public Date getData_visita() {
@@ -69,7 +79,7 @@ public class Visita {
     public void setDescrizione(String descrizione) {
         this.descrizione = descrizione;
     }
-    
+
     
 
 }
