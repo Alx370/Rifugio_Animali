e44f5a113214575136578dbfbb566e42fd8f0f1e
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-06-26 10:30:28 +0200

    Aggiunto nel controller il metodo per prendere l'id del animale e eliminarlo e nei service la logica

--- src/main/java/com/catarsi/Rifugio_Animali/controller/RifugioAnimaliMVC.java
+++ src/main/java/com/catarsi/Rifugio_Animali/controller/RifugioAnimaliMVC.java
@@ -1,7 +1,9 @@
 package com.catarsi.Rifugio_Animali.controller;
 
 import com.catarsi.Rifugio_Animali.repos.RifugioRepoAnimale;
+import com.catarsi.Rifugio_Animali.repos.RifugioRepoDiario;
 import com.catarsi.Rifugio_Animali.services.RifugioServiceAnimali;
+import com.catarsi.Rifugio_Animali.services.RifugioServiceDiario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,17 +13,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class RifugioAnimaliMVC {
 
+
+    @Autowired
     private final RifugioRepoAnimale repoAnimale;
+
+    @Autowired
+    private final RifugioRepoDiario repoDiario;
 
     @Autowired
     private RifugioServiceAnimali srvAnimale;
 
-    public RifugioAnimaliMVC(RifugioRepoAnimale repoAnimale) {
+    @Autowired
+    private RifugioServiceDiario srvDiario;
+
+    public RifugioAnimaliMVC(RifugioRepoAnimale repoAnimale, RifugioRepoDiario repoDiario) {
         this.repoAnimale = repoAnimale;
+        this.repoDiario = repoDiario;
     }
 
     @GetMapping("/animali")
-    public String addAnimali(Model m) {
+    public String getAnimali(Model m) {
         m.addAttribute("animali", srvAnimale.getAnimali());
         return "animali";
     }
@@ -30,6 +41,13 @@ public class RifugioAnimaliMVC {
     public String dettagliAnimale(@PathVariable("id") int id, Model m) {
         m.addAttribute("animale", srvAnimale.getAnimaleByIdAnimale(id));
         return "DettagliAnimali";
+    }
+
+    @GetMapping("/animali/delete/{id}")
+    public String deleteAnimale(@PathVariable("id") int id) {
+        srvDiario.deleteDiario(id);
+        srvAnimale.deleteAnimale(id);
+        return "redirect:/animali";
     }
 
 
