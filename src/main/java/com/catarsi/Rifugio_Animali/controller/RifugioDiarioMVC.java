49b8525af523efb1a53238db26d943af5837d9eb
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-05 16:22:41 +0200

    aggiunto metodo update e modifa dell'add

--- src/main/java/com/catarsi/Rifugio_Animali/controller/RifugioDiarioMVC.java
+++ src/main/java/com/catarsi/Rifugio_Animali/controller/RifugioDiarioMVC.java
@@ -1,5 +1,7 @@
 package com.catarsi.Rifugio_Animali.controller;
 
+import com.catarsi.Rifugio_Animali.services.RifugioServiceAnimali;
+import com.catarsi.Rifugio_Animali.services.RifugioServiceAnimaliImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,16 +10,16 @@ import com.catarsi.Rifugio_Animali.model.Animale;
 import com.catarsi.Rifugio_Animali.model.Diario;
 import com.catarsi.Rifugio_Animali.repos.RifugioRepoDiario;
 import com.catarsi.Rifugio_Animali.services.RifugioServiceDiarioImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class RifugioDiarioMVC {
 
     private final RifugioRepoDiario repoDiario;
+
+    @Autowired
+    private RifugioServiceAnimaliImpl srvAnimali;
 
     @Autowired
     private RifugioServiceDiarioImpl srvDiario; 
@@ -31,9 +33,7 @@ public class RifugioDiarioMVC {
         m.addAttribute("diari", srvDiario.getDiari());
         return "diari";
     }
-    
 
-    
     @GetMapping("/backoffice/diari")
     public String backofficeAnimali(Model m) {
         m.addAttribute("diari", srvDiario.getDiari());
@@ -42,8 +42,9 @@ public class RifugioDiarioMVC {
 
     @GetMapping("/backoffice/diario/add")
     public String showForm(Model model) {
-        model.addAttribute("diari", new Diario());
-        return "backofficeAggiungiDiario"; 
+        model.addAttribute("diario", new Diario());
+        model.addAttribute("animali", srvAnimali.getAnimali());
+        return "backofficeAggiungiDiario";
     }
 
     @PostMapping("/backoffice/diario/add")
@@ -51,8 +52,28 @@ public class RifugioDiarioMVC {
         srvDiario.addDiario(diario); 
         return "redirect:/backoffice/animali";
     }
-        
-    
+
+    @GetMapping("/diario/update/{id}")
+    public String mostraFormUpdate(@PathVariable("id") int id, Model model) {
+        Diario diario = srvDiario.getDiarioById(id);
+        model.addAttribute("diario", diario);
+        model.addAttribute("animali", srvAnimali.getAnimali());
+        return "backofficeUpdateDiario";
+    }
+
+    @PostMapping("/diario/update/{id}")
+    public String updateDiario(@PathVariable("id") int id, @ModelAttribute("diario") Diario aggiornato) {
+        Diario esistente = srvDiario.getDiarioById(id);
+        esistente.setStoria_animale(aggiornato.getStoria_animale());
+        esistente.setStato_salute(aggiornato.getStato_salute());
+        esistente.setStato_comportamentale(aggiornato.getStato_comportamentale());
+        esistente.setVaccinazioni(aggiornato.getVaccinazioni());
+        esistente.setOperazioni_effettuate(aggiornato.getOperazioni_effettuate());
+        srvDiario.addDiario(esistente);
+        return "redirect:/backoffice/diari";
+    }
+
+
 }
 
 
