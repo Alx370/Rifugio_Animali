55a5ad2d04d11499fc6de24b3b15275cf157a5d7
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-09 09:14:12 +0200

    fix della vista della pagina nel controller Adozioni

--- src/main/java/com/catarsi/Rifugio_Animali/controller/RifugioAdozioniMVC.java
+++ src/main/java/com/catarsi/Rifugio_Animali/controller/RifugioAdozioniMVC.java
@@ -69,7 +69,7 @@ public class RifugioAdozioniMVC {
         adozione.setData_adozione(new Date());
         animale.setData_adozione(new Date());
         srvAdozione.addAdozione(adozione);
-        return "redirect:/backoffice/adozione";
+        return "visualizzaAnimaliAdozione";
     }
 
     @GetMapping("/adozioni/visualizza")
