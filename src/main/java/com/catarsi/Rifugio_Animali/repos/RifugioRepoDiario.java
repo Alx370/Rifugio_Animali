0b084c87cad1f898af364464bbf21b7508c8908b
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-10 09:25:15 +0200

    commentato query inutilizzata, e aggiunto query per trovare l'id del diario in base all'animale

--- src/main/java/com/catarsi/Rifugio_Animali/repos/RifugioRepoDiario.java
+++ src/main/java/com/catarsi/Rifugio_Animali/repos/RifugioRepoDiario.java
@@ -20,10 +20,13 @@ public interface RifugioRepoDiario extends JpaRepository<Diario, Integer> {
     @Query("DELETE FROM Diario d WHERE d.animale.id = :id")
     void deleteByAnimaleId(@Param("id") int id);
 
-    @Modifying
-    @Transactional
-    @Query(value = "INSERT INTO diario (data, descrizione, animale_id) VALUES (:data, :descrizione, :animaleId)", nativeQuery = true)
-    void insertDiarioNative(@Param("data") LocalDate data, @Param("descrizione") String descrizione, @Param("animaleId") int animaleId);
+//    @Modifying
+//    @Transactional
+//    @Query(value = "INSERT INTO diario (data, descrizione, animale_id) VALUES (:data, :descrizione, :animaleId)", nativeQuery = true)
+//    void insertDiarioNative(@Param("data") LocalDate data, @Param("descrizione") String descrizione, @Param("animaleId") int animaleId);
+
+    @Query("SELECT d FROM Diario d WHERE d.animale.id_animale = :idAnimale")
+    Diario findByAnimaleId(@Param("idAnimale") int idAnimale);
 
 }
 
