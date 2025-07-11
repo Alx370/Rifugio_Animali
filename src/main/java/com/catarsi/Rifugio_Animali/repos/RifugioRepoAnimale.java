7789bee115169a3a58650799d214a78cf8818102
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-11 18:25:13 +0200

    aggiunto classe di riferimento per prendere i filtri nel repo

--- src/main/java/com/catarsi/Rifugio_Animali/repos/RifugioRepoAnimale.java
+++ src/main/java/com/catarsi/Rifugio_Animali/repos/RifugioRepoAnimale.java
@@ -2,8 +2,9 @@ package com.catarsi.Rifugio_Animali.repos;
 
 import com.catarsi.Rifugio_Animali.model.Animale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RifugioRepoAnimale extends JpaRepository<Animale, Integer> {}
+public interface RifugioRepoAnimale extends JpaRepository<Animale, Integer>, JpaSpecificationExecutor<Animale> {}
 
