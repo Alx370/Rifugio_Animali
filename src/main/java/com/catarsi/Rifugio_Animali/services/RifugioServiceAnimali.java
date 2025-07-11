c15a08b9afa8219d8477cfd044fcdb59a6c13862
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-11 18:27:06 +0200

    creato i metodi in RifugioServiceAnimali.java e implementati in RifugioServiceAnimaliImpl.java per adottare i filtri nella nostra applicazione

--- src/main/java/com/catarsi/Rifugio_Animali/services/RifugioServiceAnimali.java
+++ src/main/java/com/catarsi/Rifugio_Animali/services/RifugioServiceAnimali.java
@@ -1,6 +1,7 @@
 package com.catarsi.Rifugio_Animali.services;
 
 import com.catarsi.Rifugio_Animali.model.Animale;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
@@ -10,7 +11,16 @@ public interface RifugioServiceAnimali {
     void deleteAnimale(int id);
     List<Animale> getAnimali();
 
-    Animale getAnimaleByIdAnimale(int id); 
+    Animale getAnimaleByIdAnimale(int id);
+
+    Specification<Animale> nomeContains(String nome);
+    Specification<Animale> sessoEquals(String sesso);
+    Specification<Animale> specieEquals(String specie);
+    Specification<Animale> razzaContains(String razza);
+    Specification<Animale> pesoLessOrEqual(Double peso);
+    Specification<Animale> etaLessOrEqual(Integer eta);
+
+    List<Animale> filtraAnimali(String nome, String sesso, String specie, String razza, Double peso, Integer eta);
 }
 
 
