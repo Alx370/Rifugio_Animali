e8c8482d038f0c89d7a82827b814228014cc2137
Alessandro.Formicola <alessandro.formicola@edu-its.it>
2025-07-12 17:22:32 +0200

    Implementato nel login la Distinzione tra Backoffice ADMIN e USER utente tramite il DB e il SecurityConfig per permettere le rotte a certi tipi di pagine

--- src/main/java/com/catarsi/Rifugio_Animali/repos/RifugioRepoUtente.java
+++ src/main/java/com/catarsi/Rifugio_Animali/repos/RifugioRepoUtente.java
@@ -8,5 +8,4 @@ import com.catarsi.Rifugio_Animali.model.Utente;
 
 public interface RifugioRepoUtente extends JpaRepository<Utente, Integer> {
     Optional<Utente> findByEmail(String email);
-    
 }
